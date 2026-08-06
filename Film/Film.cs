659d15bb9a0aacbcdd1e727833cadb9b453256a7
Саша O. <si.ovcharuk2@gmail.com>
2026-08-06 11:43:05 +0300

    Add project files.

---
+++ Film/Film.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Film
+{
+    internal class Film
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Studio { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public int Duration { get; set; } = 0;
+        public int ReleaseYear { get; set; } = 0;
+
+        public Film(string name, string studio, string genre, int duration, int releaseYear)
+        {
+            Name = name;
+            Studio = studio;
+            Genre = genre;
+            Duration = duration;
+            ReleaseYear = releaseYear;
+        }
+
+        ~Film()
+        {
+            Console.WriteLine($"Деструктор для фільму: {Name}");
+        }
+
+        public override string ToString()
+        {
+            return $"Назва: {Name}, Студія: {Studio}, Жанр: {Genre}, Тривалість: {Duration} хв, Рік випуску: {ReleaseYear}";
+        }
+    }
+}
---
+++ Film/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+namespace Film
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateAndUseFilms();
+            Console.WriteLine();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        static void CreateAndUseFilms()
+        {
+            Film[] films =
+            {
+                new Film("Inception", "Warner Bros.", "Science Fiction", 148, 2010),
+                new Film("The Godfather", "Paramount Pictures", "Crime", 175, 1972),
+                new Film("The Dark Knight", "Warner Bros.", "Action", 152, 2008),
+                new Film("Pulp Fiction", "Miramax", "Crime", 154, 1994),
+                new Film("The Shawshank Redemption", "Columbia Pictures", "Drama", 142, 1994)
+            };
+
+            foreach (var film in films)
+            {
+                Console.WriteLine(film);
+            }
+        }
+    }
+}
